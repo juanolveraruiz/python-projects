06b201973bf0228ed64004bd96b1b227feea22c8
Juan <juanolveraruiz@gmail.com>
2023-09-02 22:50:52 -0600

    madlibs.py

---
+++ freeCodeCamp/madlibs.py
@@ -0,0 +1,14 @@
+# youtuber = "juan olvera"
+
+# print("suscribe to " + youtuber)
+# print("suscribe to {}".format(youtuber))
+# print(f"sucribe to {youtuber}")
+
+adj = input("Adjetive: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It made me so excited all the time because i love to {verb1}. Stay hydrated and {verb2} like you are {famous_person} "
+
+print(madlib)
